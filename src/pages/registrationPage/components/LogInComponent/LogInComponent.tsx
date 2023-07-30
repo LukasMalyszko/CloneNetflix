179b4e740fe71fd9f2e2d29c879cd4e945160fbe
Lukasz Malyszko <lukas.malyszko@gmail.com>
2023-07-30 15:20:29 +0200

    add login func

--- src/pages/registrationPage/components/LogInComponent/LogInComponent.tsx
+++ src/pages/registrationPage/components/LogInComponent/LogInComponent.tsx
@@ -4,6 +4,9 @@ import { LogInButton } from "../LogInButton";
 import { LogInputContainer } from "../LogInputContainer/LogInputContainer";
 import { LogoNetflix } from "../LogoNetflix";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth, showLoginError } from "../../../../config/firebase";
 
 export const LogInComponent = () => {
   const emailLabel = "Email address or username";
@@ -12,6 +15,95 @@ export const LogInComponent = () => {
 
   const handleLoader = () => {
     setDataLoading(1);
+  };
+
+  interface UserProps {
+    [key: string]: {
+      value: string;
+      isValid: boolean;
+      errorMessage: string;
+    };
+  }
+
+  const [form, setForm] = useState<UserProps>({
+    email: {
+      value: "",
+      isValid: false,
+      errorMessage: "",
+    },
+    password: {
+      value: "",
+      isValid: false,
+      errorMessage: "",
+    },
+  });
+
+  const handleChange = (value: string, name: string) => {
+    const validator = basicValidator[name];
+
+    let isValid = true;
+    let errorMessage = "";
+
+    if (validator) {
+      const validatorValue = validator(value);
+
+      if (typeof validatorValue === "string") {
+        isValid = !validatorValue;
+        errorMessage = validatorValue;
+      }
+    }
+
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: {
+        ...prevForm[name],
+        value,
+        isValid,
+        errorMessage,
+      },
+    }));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handleChange(form.email.value, "email");
+    handleChange(form.password.value, "password");
+    // if (isFormValid) {
+    console.log(form);
+    // }
+  };
+
+  interface IBasicValidator {
+    [key: string]: (value: string) => boolean | string | void;
+  }
+
+  const basicValidator: IBasicValidator = {
+    email: (value) =>
+      /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(String(value)) ||
+      "Incorrect e-mail",
+
+    password: (value) =>
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(String(value)) ||
+      "Incorrect e-mail or password ",
+  };
+
+  const handleChangeInput = (value: string, name: string) => {
+    handleChange(value, name);
+  };
+
+  const navigate = useNavigate();
+
+  const loginEmailPassword = async () => {
+    const loginEmail = form.email.value;
+    const LoginPassword = form.password.value;
+    try {
+      await signInWithEmailAndPassword(auth, loginEmail, LoginPassword);
+      navigate("/dashboard");
+    } catch (error: any) {
+      alert(error.message);
+      showLoginError(error.message);
+      console.log(error.message);
+    }
   };
 
   return (
@@ -34,41 +126,59 @@ export const LogInComponent = () => {
           text="Log in with Google"
         />
         <BreakLine />
-        <div className="log-component__login-inputs">
-          <LogInputContainer
-            label={emailLabel}
-            type={"text"}
-            placeholder={emailLabel}
-          />
-          <LogInputContainer
-            label={"Password"}
-            type={"password"}
-            placeholder={"Password"}
-          />
-        </div>
-        <div className="log-component__link">
-          <a href="#">Do not you remember the password?</a>
-        </div>
-        <div className="log-component__checkbox-container">
-          <input
-            className="log-component__checkbox"
-            id="remember-me"
-            type="checkbox"
-          />
-          <label htmlFor="remember-me">Remember me</label>
-        </div>
-        <button
-          type="submit"
-          className="log-component__primary-button"
-          data-loader={dataLoading}
-          onClick={handleLoader}
-          disabled={true}
-        >
-          <div className="text">Log in</div>
-          <div className="loader">
-            <img src="/animated-loader.svg" />
+        <form className="log-component__login-form" onSubmit={handleSubmit}>
+          <div className="log-component__inputs">
+            <LogInputContainer
+              label={emailLabel}
+              type={"text"}
+              placeholder={emailLabel}
+              onChange={(value) => handleChangeInput(value, "email")}
+            />
+
+            <LogInputContainer
+              label={"Password"}
+              type={"password"}
+              placeholder={"Password"}
+              onChange={(value) => handleChangeInput(value, "password")}
+            />
+            {form.password.errorMessage && (
+              <div className="log-component__error-container">
+                <div className="log-component__error-img-box">
+                  <img src="/warning 3.svg" alt="" />
+                </div>
+                <p className="log-component__error-message">
+                  {form.password.errorMessage}
+                </p>
+              </div>
+            )}
           </div>
-        </button>
+          <div className="log-component__link">
+            <a href="#">Do not you remember the password?</a>
+          </div>
+          <div className="log-component__checkbox-container">
+            <input
+              className="log-component__checkbox"
+              id="remember-me"
+              type="checkbox"
+            />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          <button
+            type="submit"
+            className="log-component__primary-button"
+            data-loader={dataLoading}
+            onClick={() => {
+              handleLoader();
+              loginEmailPassword();
+            }}
+            disabled={false}
+          >
+            <div className="text">Log in</div>
+            <div className="loader">
+              <img src="/animated-loader.svg" />
+            </div>
+          </button>
+        </form>
         <div className="log-component__break-line"></div>
         <div className="log-component__footer-text">
           You dont have an account yet?
--- src/pages/registrationPage/components/LogInputContainer/LogInputContainer.tsx
+++ src/pages/registrationPage/components/LogInputContainer/LogInputContainer.tsx
@@ -1,17 +1,32 @@
 import "./LogInputContainer.scss";
 
 interface LogInputContainerProps {
-    label: string,
-    type: string,
-    placeholder: string,
+  label: string;
+  type: string;
+  placeholder: string;
+  onChange: (value: string) => void;
 }
 
-export const LogInputContainer: React.FC<LogInputContainerProps> = ( {label, type, placeholder}) => {
+export const LogInputContainer: React.FC<LogInputContainerProps> = ({
+  label,
+  type,
+  placeholder,
+  onChange,
+}) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    onChange(value);
+  }
+
   return (
     <div className="log-input-container">
-            <label className="log-input-container__label">{label}</label>
-            <input className="log-input-container__input" type={type} 
-            placeholder={placeholder}/>
-        </div>
-  )
-}
+      <label className="log-input-container__label">{label}</label>
+      <input
+        className="log-input-container__input"
+        type={type}
+        placeholder={placeholder}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
