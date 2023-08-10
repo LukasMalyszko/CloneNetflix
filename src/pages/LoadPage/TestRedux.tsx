e33e9330c87098bd5ba4f7539862da654d200680
Lukasz Malyszko <lukas.malyszko@gmail.com>
2023-08-10 17:34:22 +0200

    LogOut

--- src/pages/LoadPage/TestRedux.tsx
+++ src/pages/LoadPage/TestRedux.tsx
@@ -2,7 +2,7 @@ import { increment, decrement } from "../../redux/counter";
 import "./TestRedux.scss";
 import { useSelector, useDispatch } from "react-redux";
 import { TestRedux2 } from "./TestRedux2";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 interface StateProps {
   counter: {
